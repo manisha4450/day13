e114e8a8c1181599d779d100e68a9bc22e5b537d
manisha4450 <chowdaryc226@gmail.com>
2024-12-15 10:58:45 +0530

    Add files via upload

---
+++ lambda1.java
@@ -0,0 +1,8 @@
+public class lambda1{
+    public static void main(String[] args){
+        Runnable r=()->System.out.println("i am new from java8 version");
+        Thread t=new Thread(r);
+        t.start();
+        System.out.println(t.isAlive());
+    }
+}
